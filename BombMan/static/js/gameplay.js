19abc53ca6bbe86188feaaa9f821c443971c385b
Senpaistorm <austin.li@mail.utoronto.ca>
2019-02-26 19:48:49 -0500

    Created a function that generates an empty gameboard

--- BombMan/static/js/gameplay.js
+++ BombMan/static/js/gameplay.js
@@ -5,11 +5,24 @@
 	 * The game plug-in interface that use to implement and register games
 	 * with Core. This class is where all the game logic from
 	 */
+	const GAMEBOARD_SIZE = 15;
+
 	function Gameplay() {
-		this.gameboard = [];
-		for(var i = 0; i < 15; i++){
-			this.gameboard.push([1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+		this.gameboard = emptyGameboard();
+		this.gameboard[7][8] = 1;
+		this.gameboard[10][11] = 1;
+	}
+
+	function emptyGameboard(){
+		let gameboard = []
+		for(let i = 0; i < GAMEBOARD_SIZE; i++){
+			let arr = [];
+			for (var j = 0; j < GAMEBOARD_SIZE; j++){
+				arr.push(0)
+			}
+			gameboard.push(arr);
 		}
+		return gameboard;
 	}
 
     // Export to window
