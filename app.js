a37f69f739a7ded9a996e0c57c73ed666fd1e580
Senpaistorm <austin.l@qq.com>
2020-01-11 19:08:28 -0500

    Using 0.0.0.0 as port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const Util = require('./lib/Util');
 
 // setup ports
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
